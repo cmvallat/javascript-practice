2da8822adb1dd49873ada2646195e6d4a3b92a79
Christian Vallat <cmvallat@live.unc.edu>
2021-11-02 16:40:28 -0400

    Added button click functionality, logging variables

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
+let first = "Christian";
+let last = "Vallat";
+let button = document.getElementById("name");
+const div = document.getElementById("div1");
 
+let hobbies = [
+  "Watching my favorite sports teams",
+  "Eating good pasta",
+  "Listening to country music",
+];
+
+//Log on page load
+console.log("Hi, my name is " + first + " " + last);
+console.log("Some of my favorite hobbies include:");
+hobbies.forEach((hobby) => {
+  console.log(hobby);
+});
+
+//Log and append on button click
+function printNames() {
+  console.log(first + " " + last);
+  let para = document.createElement("p");
+  let text = document.createTextNode(first + " " + last);
+  para.appendChild(text);
+  div.appendChild(para);
+}
+
+button.addEventListener("click", printNames);
